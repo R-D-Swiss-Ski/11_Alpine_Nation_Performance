0b1ea38fc2b94967a1e19ccefba61a3171483411
ElenaGrafUZH <elena.graf@students.unibe.ch>
2025-10-13 15:21:25 +0200

    fix path

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [-f /.streamlit/secrets.toml] then
-    cp /.streamlit/secrets.toml /root/.streamlit/secrets.toml
-    echo "secrets.toml copied to /root/.streamlit/"
+    cp ../.streamlit/secrets.toml ../root/.streamlit/secrets.toml
+    echo "secrets.toml copied to root"
 else
     echo "Something went wrong when copying secrets.toml to root"
 fi
