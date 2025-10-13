d9aaf3c1651da5bf737fd90a4aaef7ce2067e719
ElenaGrafUZH <elena.graf@students.unibe.ch>
2025-10-13 15:45:35 +0200

    logging

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -f /.streamlit/secrets.toml ]; then
+LOG_FILE="/startup.log"
+pwd >> ${LOG_FILE}
+
+if [ -f ../.streamlit/secrets.toml ]; then
     cp ../.streamlit/secrets.toml ../root/.streamlit/secrets.toml
-    echo "secrets.toml copied to root"
-else
-    echo "Something went wrong when copying secrets.toml to root"
+    echo "secrets.toml copied to root" | tee -a "$LOG_FILE"
+    echo "Something went wrong when copying secrets.toml to root" | tee -a "$LOG_FILE"
 fi
 
