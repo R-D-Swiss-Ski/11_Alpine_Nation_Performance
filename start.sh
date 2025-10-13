61a8047c4cb56eb69d13970cca50ce95ac9795ae
ElenaGrafUZH <elena.graf@students.unibe.ch>
2025-10-13 15:49:21 +0200

    fix paths

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-LOG_FILE="/startup.log"
+LOG_FILE="/app/startup.log"
 pwd >> ${LOG_FILE}
 
-if [ -f ../.streamlit/secrets.toml ]; then
-    cp ../.streamlit/secrets.toml ../root/.streamlit/secrets.toml
+if [ -f /.streamlit/secrets.toml ]; then
+    cp /.streamlit/secrets.toml /root/.streamlit/secrets.toml
     echo "secrets.toml copied to root" | tee -a "$LOG_FILE"
     echo "Something went wrong when copying secrets.toml to root" | tee -a "$LOG_FILE"
 fi
